0aa10dca9a5b895c3e4b02faa6f364878c58f846
p-t-a-p-1 <kazu.hakozaki@ma-vericks.com>
2024-07-13 16:37:14 +0900

    update shadcn/uiのインストール

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,37 +1,37 @@
-import { getArticleBySlug, getArticles } from "@/lib/newt";
-import styles from "@/app/page.module.css";
+import { getArticleBySlug, getArticles } from '@/lib/newt'
+import styles from '@/app/page.module.css'
 
 type Props = {
   params: {
-    slug: string;
-  };
-};
+    slug: string
+  }
+}
 
 // ビルド時に静的にルートを生成
 export async function generateStaticParams() {
-  const articles = await getArticles();
+  const articles = await getArticles()
   return articles.map((article) => ({
     slug: article.slug,
-  }));
+  }))
 }
 // generateStaticParamsで定義されていないパスにアクセスされた場合、404を返す
-export const dynamicParams = false;
+export const dynamicParams = false
 
 export async function generateMetadata({ params }: Props) {
-  const { slug } = params;
-  const article = await getArticleBySlug(slug);
+  const { slug } = params
+  const article = await getArticleBySlug(slug)
 
   return {
     title: article?.title,
-    description: "投稿詳細ページです",
-  };
+    description: '投稿詳細ページです',
+  }
 }
 
 export default async function Article({ params }: Props) {
-  const { slug } = params;
-  const article = await getArticleBySlug(slug);
+  const { slug } = params
+  const article = await getArticleBySlug(slug)
   if (!article) {
-    return <div>記事が見つかりません</div>;
+    return <div>記事が見つかりません</div>
   }
 
   return (
@@ -40,5 +40,5 @@ export default async function Article({ params }: Props) {
       {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
       <div dangerouslySetInnerHTML={{ __html: article.body }} />
     </main>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,33 @@
-import { getArticles } from "@/lib/newt";
-import styles from "@/app/page.module.css";
-import { Metadata } from "next";
-import Link from "next/link";
+import { Button } from '@/components/ui/button'
+import { getArticles } from '@/lib/newt'
+import { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
-  title: "Newt/Next.jsブログ",
-  description: "Newt/Next.jsを利用したブログです",
-};
+  title: 'Newt/Next.jsブログ',
+  description: 'Newt/Next.jsを利用したブログです',
+}
 
 export default async function Home() {
-  const articles = await getArticles();
+  const articles = await getArticles()
 
   return (
-    <main className={styles.main}>
+    <main>
+      <h1 className="text-2xl font-bold underline">記事一覧</h1>
+      <Button>ボタン</Button>
       <ul>
         {articles.map((article) => {
-          const hasUrl = !!article.url;
-          const url = hasUrl ? article.url : `articles/${article.slug}`;
+          const hasUrl = !!article.url
+          const url = hasUrl ? article.url : `articles/${article.slug}`
           return (
             <li key={article._id}>
-              <Link href={url} target={hasUrl ? "_blank" : ""}>
+              <Link href={url} target={hasUrl ? '_blank' : ''}>
                 {article.title}
               </Link>
             </li>
-          );
+          )
         })}
       </ul>
     </main>
-  );
+  )
 }
