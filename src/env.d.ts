5ec28670500e403fbf907d0eb945dc7ee158d497
p-t-a-p-1 <kazu.hakozaki@ma-vericks.com>
2025-08-05 12:57:06 +0900

    update NEWT依存解消

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,1 @@
-interface ImportMetaEnv {
-  readonly NEWT_SPACE_UID: string;
-  readonly NEWT_CDN_API_TOKEN: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+// Environmental variables are no longer needed as we've migrated to local Markdown content management
